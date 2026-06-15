635a42e83bee4eb47d045aabb5d6558b71bc5c3c
priyadharsanofficial2026 <priyadharsan.official@gmail.com>
2026-06-15 19:12:50 +0530

    file added

--- tests/Sample.spec.js
+++ tests/Sample.spec.js
@@ -3,7 +3,7 @@ import {test} from '@playwright/test';
 test('google login page', async({page}) =>{
 await page.goto('https://www.instagram.com/?hl=en')
 await page.locator('//input[@name="email"]').type('Dharsan0105')
-await page.locator('//input[@name="pass"]').type('Priyan@1996')
+await page.locator('//input[@name="pass"]').type('Priyan@199')
 await page.locator('//span[text()="Log in"]').click();
 await page.waitForTimeout(3000)
 });
